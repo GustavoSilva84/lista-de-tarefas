158985e8ea9124101590a1df04467d0a213cdf1e
Gustavo-Silva01 <fogofracoo@gmail.com>
2022-02-17 09:33:23 -0300

    estilização da pagina criar conta

--- pages/createAccount/index.tsx
+++ pages/createAccount/index.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { useRef } from "react";
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
+
+import { 
+  Text, 
+  TextInput
+} from 'react-native';
+
+import { 
+  Input, 
+  Label, 
+  Icon,
+  Form,
+  ButtonBlack, 
+  TextButtonBlack,
+  ButtonTransparent,
+  TextButtonTransparent
+} from "../../components/StandardComponents";
+
+import { View } from "./style";
 
 export default function Login({ navigation }: NativeStackHeaderProps) {
 
+  const RefInputEmail = useRef<TextInput>(null);
+  const RefInputPassword = useRef<TextInput>(null);
+
   return (  
     <View>
-      <Text> Create an account </Text>
-      <Button
-        title="Login"
-        onPress={() => navigation.navigate("login")}
-      />
+      <Icon source={require("../../assets/myIcon.png")}/>
+
+      <Form>
+        <Label onPress={() => RefInputEmail?.current?.focus()}> E-Mail </Label>
+        <Input ref={RefInputEmail}/>
+
+        <Label onPress={() => RefInputPassword?.current?.focus()}> E-Mail </Label>
+        <Input ref={RefInputPassword}/>
+
+        <ButtonBlack onPress={() => navigation.navigate("createAccount")}> 
+          <TextButtonBlack>  Criar conta </TextButtonBlack>
+        </ButtonBlack>
+
+        <ButtonTransparent onPress={() => navigation.navigate("login")}> 
+          <TextButtonTransparent> Logar </TextButtonTransparent>
+        </ButtonTransparent>
+      </Form>
     </View>
   )
 
---
+++ pages/createAccount/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native'
+
+export const View = styled.View`
+  flex: 1;
+  display: flex;
+  background-color: #FFFFFF;
+  padding: 0 10px;
+  align-items: center;
+`
